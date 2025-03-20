a0836049d1b760be3709b17127d53fe2df59b7b2
MaicolZ04 <zapatamaicol95@gmail.com>
2025-03-20 16:41:25 -0500

    V0.3

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ menuData.forEach(item => {
     a.classList.add('nav-link');
     a.textContent = item.name;
     
-    // Si tiene submenú, agregarlo
     if (item.submenu && item.submenu.length > 0) {
         const submenu = document.createElement('ul');
         submenu.classList.add('submenu-list');
@@ -56,3 +55,49 @@ menuData.forEach(item => {
     menuList.appendChild(li);
 });
 
+particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#0056b3"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#0056b3",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        }
+      }
+    }
+  });
+  
