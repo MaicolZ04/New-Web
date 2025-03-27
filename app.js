83be9bd8bd3cadc8783c95cc3df3e63946340a9c
MaicolZ04 <zapatamaicol95@gmail.com>
2025-03-27 01:58:59 -0500

    V0.4.1

--- app.js
+++ app.js
@@ -1,58 +1,50 @@
-// Datos del menú (simulando una base de datos o API)
-const menuData = [
-    { name: 'INICIO', url: 'https://www.americankpo.com/' },
-    { name: 'NOSOTROS', url: 'https://www.americankpo.com/nosotros/' },
-    { 
-        name: 'SERVICIOS', 
-        url: 'https://www.americankpo.com/servicios/',
-        submenu: [
-            { name: 'Auditoría Médica', url: 'https://www.americankpo.com/auditoria-medica/' },
-            { name: 'Cuentas Médicas', url: 'https://www.americankpo.com/cuentas-medica/' },
-            { name: 'Gestión Recobros', url: 'https://www.americankpo.com/gestion-recobros/' },
-        ]
-    },
-    { name: 'CLIENTES', url: 'https://www.americankpo.com/clientes/' },
-    { name: 'CONTACTO', url: 'https://www.americankpo.com/contacto/' }
-];
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.nav');
+  const body = document.body;
+  
+  menuToggle.addEventListener('click', function() {
+      nav.classList.toggle('active');
+      body.classList.toggle('menu-open');
+      document.body.style.overflow = nav.classList.contains('active') ? 'hidden' : '';
+  });
+  
+  // Cerrar menú al hacer clic en un enlace
+  document.querySelectorAll('.nav a, .nav .btn-empezar').forEach(item => {
+      item.addEventListener('click', () => {
+          nav.classList.remove('active');
+          body.classList.remove('menu-open');
+          document.body.style.overflow = '';
+      });
+  });
+});
+  
+  window.addEventListener('load', () => {
+    const videoSection = document.querySelector('.video-section');
+    videoSection.classList.add('fade-in');
+  });
 
-// Datos de redes sociales (simulando una base de datos o API)
-const socialLinks = [
-    { name: 'Facebook', url: 'https://www.facebook.com/AmericanKPOLatam/', icon: 'facebook' },
-    { name: 'Twitter', url: 'https://twitter.com/american_kpo', icon: 'twitter' },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/company/american-kpo/', icon: 'linkedin' }
-];
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
-// Cargar el menú dinámicamente
-const menuList = document.getElementById('menuList');
-menuData.forEach(item => {
-    const li = document.createElement('li');
-    li.classList.add('nav-item');
-    
-    const a = document.createElement('a');
-    a.href = item.url;
-    a.classList.add('nav-link');
-    a.textContent = item.name;
-    
-    if (item.submenu && item.submenu.length > 0) {
-        const submenu = document.createElement('ul');
-        submenu.classList.add('submenu-list');
-        
-        item.submenu.forEach(sub => {
-            const submenuItem = document.createElement('li');
-            submenuItem.classList.add('submenu-item');
-            const submenuLink = document.createElement('a');
-            submenuLink.href = sub.url;
-            submenuLink.classList.add('submenu-link');
-            submenuLink.textContent = sub.name;
-            submenuItem.appendChild(submenuLink);
-            submenu.appendChild(submenuItem);
-        });
-        
-        li.appendChild(submenu);
-    }
-    
-    li.appendChild(a);
-    menuList.appendChild(li);
+window.addEventListener('scroll', () => {
+  const preProductosH1 = document.querySelector('.preProductos h1');
+  const preProductosP = document.querySelector('.preProductos p');
+
+  if (isElementInViewport(preProductosH1)) {
+    preProductosH1.classList.add('fade-in');
+  }
+
+  if (isElementInViewport(preProductosP)) {
+    preProductosP.classList.add('fade-in'); 
+  }
 });
 
 particlesJS('particles-js', {
